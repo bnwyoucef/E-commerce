655a1fdce0968ffa9d4cac4297d0636d6eb1dc20
bnwyoucef <y.bounoua@esi-sba.dz>
2022-06-29 23:19:02 +0100

    add navbar && footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import {AiFillInstagram,AiOutlineTwitter} from 'react-icons/ai';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2022 YouNou headphones All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
